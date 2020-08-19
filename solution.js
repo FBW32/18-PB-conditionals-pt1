92fc3badc5c9c2524acb24d3c96d7a6329811fa6
Misbah-D <misbah.deeb@gmail.com>
2020-08-19 14:54:18 +0200

    ok

---
+++ solution.js
@@ -0,0 +1,36 @@
+// Check if two given integer values are in the range 50 to 99 (this includes both 50 and 99). Print true if either of them are in the range.
+let integer1 = 50
+
+    if ((integer1 >= 50 && integer1 <= 99))
+    {
+     console.log(true);
+} 
+
+// Check if three given integer values are in the range 50 to 99 (inclusive). Print true if one or more of them are in the range.
+
+
+// Declare the variables a, b and c, and give them number values. Check which one out of the three variables has the largest value and print it to the console. Then change the values of the variables to see if your conditional still works.
+
+
+// Create a new string adding "Py" in front of a given string. If the given string begins with "Py" then print the original string.
+
+
+// Calculate the sum of the two integers. If the sum is in the range 50 to 80, print 65, otherwise print 80.
+
+
+// Check whether the sum of two integers is 8, or whether their difference is 8. If one of these is the case, print true.
+
+
+// Check whether either one of two integers is 15, or if their sum is 15. If one of these is the case, print true.
+
+
+// Check whether one of two integers is a multiple of 7 or 11. If so, print true.
+
+
+// Calculate the sum of the two given integers. If the two values are the same, then print triple their sum.
+
+
+// Calculate the difference between a specified number and 19. Print double the difference if the specified number is greater than 19.
+
+let
+// BONUS CHALLENGE: Play around with each of your results, i.e., make sure they are dynamic to different inputs; is there a more efficient way of writing your code?; etc.
